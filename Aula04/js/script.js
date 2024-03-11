a36c7f50055a96abf9ebfc2e183913eadad59480
PedroHMedd <pedromedeirosetec02@gmail.com>
2024-03-11 20:58:34 -0300

    calculadora de imc e css

--- Aula04/js/script.js
+++ Aula04/js/script.js
@@ -1,36 +1,54 @@
-const calcular = document.getElementById ('calcular')
-
-
-function imc() {
-    const altura = document.getElementById ('altura')
-    const peso = document.getElementById ('peso')
-    const resultado = document.getElementById ('resultado')
-
-    if (altura !== '' && peso !=='') {
-
-        const valorIMC = (peso.value/ (altura.value * altura.value)).toFixed(2);
-        
-        let classificacao = '';
-
-        if(valorIMC < 18.5){
-            classificacao = 'Baixo peso'
-        }else if (valorIMC < 25){
-            classificacao = 'Intervalo normal'
-        }else if (valorIMC < 30){
-            classificacao = 'Sobrepeso'
-        }else if (valorIMC < 35){
-            classificacao = 'Obesidade classe I'
-        }else if (valorIMC < 40){
-            classificacao = 'Obesidade classe II'
-        }else {
-            classificacao = 'Obesidade classe III';
-        }
-
-        resultado.textContent = `Seu IMC É ${valorIMC} Seu diagnóstico é ${classificacao}`;
-    }else {
-        resultado.innerHTML = 'Por favor preencha todos os campos!';
-    }
-
+function calcular() {
+    var peso = parseFloat(document.getElementById('peso').value)
+    var altura = parseFloat(document.getElementById('altura').value)
+    var calcular = peso / (altura * altura)
+    resultado.innerHTML = calcular
+    d(calcular)
 }
 
-calcular.addEventListener('click', imc);
+function d(calcular) {
+    var genero = document.getElementById('genero').value
+    if (genero == 'masculino') {
+        if (calcular < 20) {
+            dvalor.innerHTML = 'Abaixo do peso normal'
+        }
+        else if (calcular >= 20 && calcular < 25) {
+            dvalor.innerHTML = 'Normal'
+        }
+        else if (calcular >= 25 && calcular < 30) {
+            dvalor.innerHTML = 'Obesidade leve'
+        }
+        else if (calcular >= 30 && calcular < 35) {
+            dvalor.innerHTML = 'Obesidade grau I'
+        }
+        else if (calcular >= 40) {
+            dvalor.innerHTML = 'Obesidade grau III'
+        }
+        else {
+            dvalor.innerHTML = 'Obesidade grau II'
+        }
+    }
+    else if (genero == 'feminino') {
+        if (calcular < 19) {
+            dvalor.innerHTML = 'Abaixo do peso normal'
+        }
+        else if (calcular >= 19 && calcular < 24) {
+            dvalor.innerHTML = 'Normal'
+        }
+        else if (calcular >= 24 && calcular < 29) {
+            dvalor.innerHTML = 'Obesidade leve'
+        }
+        else if (calcular >= 29 && calcular < 34) {
+            dvalor.innerHTML = 'Obesidade grau I'
+        }
+        else if (calcular >= 39) {
+            dvalor.innerHTML = 'Obesidade grau III'
+        }
+        else {
+            dvalor.innerHTML = 'Obesidade grau II'
+        }
+    }
+    else {
+        dvalor.innerHTML = 'Erro de dados'
+    }
+}

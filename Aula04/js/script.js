de116ef671c9fe117c0495b97056a3cc625596bf
PedroHMedd <pedromedeirosetec02@gmail.com>
2024-03-12 21:33:46 -0300

    js

--- Aula04/js/script.js
+++ Aula04/js/script.js
@@ -10,13 +10,13 @@ function d(calcular) {
     var genero = document.getElementById('genero').value
     if (genero == 'masculino') {
         if (calcular < 20) {
-            dvalor.innerHTML = 'Abaixo do peso normal'
+            dvalor.innerHTML = 'Abaixo do peso'
         }
         else if (calcular >= 20 && calcular < 25) {
             dvalor.innerHTML = 'Normal'
         }
         else if (calcular >= 25 && calcular < 30) {
-            dvalor.innerHTML = 'Obesidade leve'
+            dvalor.innerHTML = 'Sobrepeso'
         }
         else if (calcular >= 30 && calcular < 35) {
             dvalor.innerHTML = 'Obesidade grau I'
@@ -30,13 +30,13 @@ function d(calcular) {
     }
     else if (genero == 'feminino') {
         if (calcular < 19) {
-            dvalor.innerHTML = 'Abaixo do peso normal'
+            dvalor.innerHTML = 'Abaixo do peso'
         }
         else if (calcular >= 19 && calcular < 24) {
             dvalor.innerHTML = 'Normal'
         }
         else if (calcular >= 24 && calcular < 29) {
-            dvalor.innerHTML = 'Obesidade leve'
+            dvalor.innerHTML = 'obrepeso'
         }
         else if (calcular >= 29 && calcular < 34) {
             dvalor.innerHTML = 'Obesidade grau I'
@@ -49,6 +49,6 @@ function d(calcular) {
         }
     }
     else {
-        dvalor.innerHTML = 'Erro de dados'
+        dvalor.innerHTML = 'Erro'
     }
 }

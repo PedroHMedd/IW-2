2082949d0a444d2253a5b78ebb16092b08638abd
PedroHMedd <pedromedeirosetec02@gmail.com>
2024-03-14 20:12:34 -0300

    js

--- Aula04/js/script.js
+++ Aula04/js/script.js
@@ -2,53 +2,41 @@ function calcular() {
     var peso = parseFloat(document.getElementById('peso').value)
     var altura = parseFloat(document.getElementById('altura').value)
     var calcular = peso / (altura * altura)
-    resultado.innerHTML = calcular
+    resultado.innerHTML = calcular.toFixed(1)
     d(calcular)
 }
 
 function d(calcular) {
-    var genero = document.getElementById('genero').value
+    var genero = document.getElementById('genero').value;
     if (genero == 'masculino') {
         if (calcular < 20) {
-            dvalor.innerHTML = 'Abaixo do peso'
+            dvalor.innerHTML = 'Abaixo do peso';
+        } else if (calcular >= 20 && calcular < 25) {
+            dvalor.innerHTML = 'Peso ideal';
+        } else if (calcular >= 25 && calcular < 30) {
+            dvalor.innerHTML = 'Sobrepeso';
+        } else if (calcular >= 30 && calcular < 35) {
+            dvalor.innerHTML = 'Obesidade grau I';
+        } else if (calcular >= 40) {
+            dvalor.innerHTML = 'Obesidade grau III';
+        } else {
+            dvalor.innerHTML = 'Obesidade grau II';
         }
-        else if (calcular >= 20 && calcular < 25) {
-            dvalor.innerHTML = 'Normal'
-        }
-        else if (calcular >= 25 && calcular < 30) {
-            dvalor.innerHTML = 'Sobrepeso'
-        }
-        else if (calcular >= 30 && calcular < 35) {
-            dvalor.innerHTML = 'Obesidade grau I'
-        }
-        else if (calcular >= 40) {
-            dvalor.innerHTML = 'Obesidade grau III'
-        }
-        else {
-            dvalor.innerHTML = 'Obesidade grau II'
-        }
-    }
-    else if (genero == 'feminino') {
+    } else if (genero == 'feminino') {
         if (calcular < 19) {
-            dvalor.innerHTML = 'Abaixo do peso'
+            dvalor.innerHTML = 'Abaixo do peso';
+        } else if (calcular >= 19 && calcular < 24) {
+            dvalor.innerHTML = 'Peso ideal';
+        } else if (calcular >= 24 && calcular < 29) {
+            dvalor.innerHTML = 'Sobrepeso';
+        } else if (calcular >= 29 && calcular < 34) {
+            dvalor.innerHTML = 'Obesidade grau I';
+        } else if (calcular >= 34 && calcular < 39) {
+            dvalor.innerHTML = 'Obesidade grau II';
+        } else {
+            dvalor.innerHTML = 'Obesidade grau III';
         }
-        else if (calcular >= 19 && calcular < 24) {
-            dvalor.innerHTML = 'Normal'
-        }
-        else if (calcular >= 24 && calcular < 29) {
-            dvalor.innerHTML = 'obrepeso'
-        }
-        else if (calcular >= 29 && calcular < 34) {
-            dvalor.innerHTML = 'Obesidade grau I'
-        }
-        else if (calcular >= 39) {
-            dvalor.innerHTML = 'Obesidade grau III'
-        }
-        else {
-            dvalor.innerHTML = 'Obesidade grau II'
-        }
-    }
-    else {
-        dvalor.innerHTML = 'Erro'
+    } else {
+        dvalor.innerHTML = 'Erro';
     }
 }
